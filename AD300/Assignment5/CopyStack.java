bb7f8111c3eac24a9d2acb313dd358427feaedb1
Scott H <zenwattage@gmail.com>
2020-11-20 11:59:50 -0800

    restarting ta5

--- AD300/Assignment5/CopyStack.java
+++ AD300/Assignment5/CopyStack.java
@@ -8,7 +8,7 @@ class Copystack {
 
     public static Stack<Integer> copyStack(Stack<Integer> s) {
         Stack<Integer> copy = new Stack<Integer>();
-
+        copy.addAll(s);
         return copy;
     }
 
@@ -17,7 +17,30 @@ class Copystack {
      * @param args
      */
     public static void main(String[] args) {
+        Stack<Integer> og = new Stack<Integer>();
+        og.push(5);
+        og.push(8);
+        og.push(16);
+        og.push(7);
+        og.push(9);
 
+        System.out.println("Original stack = " + og);
+
+        System.out.println("The copy stack = " + copyStack(og));
+
+        System.out.println("The original stack = " + og);
+
+        /**
+         * Sample runs:
+         * 
+         * Original stack = [5, 8, 16, 7, 9]
+         * 
+         * The copy stack = [5, 8, 16, 7, 9]
+         * 
+         * Original stack = [5, 8, 16, 7, 9]
+         * 
+         * The mirror Stack is : [5, 8, 16, 7, 9, 9, 7, 16, 8, 5]
+         */
     }
 
 }
