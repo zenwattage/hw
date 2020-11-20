b23d052145c54d818d5295a4dc59f58f20bfc056
Scott H <zenwattage@gmail.com>
2020-11-20 11:36:16 -0800

    initial push of AD300

---
+++ AD300/Assignment5/CopyStack.java
@@ -0,0 +1,23 @@
+import java.util.Stack;
+
+/**
+ * @author Scott Hansford
+ * @version TA5
+ */
+class Copystack {
+
+    public static Stack<Integer> copyStack(Stack<Integer> s) {
+        Stack<Integer> copy = new Stack<Integer>();
+
+        return copy;
+    }
+
+    /**
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
