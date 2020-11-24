97dbbb1d216e28e21e6c455cc11bed27c2373700
zenwattage <zenwattage@gmail.com>
2020-11-23 21:28:34 -0800

    add

---
+++ AD300/PlayGround/ch15.java
@@ -0,0 +1,12 @@
+import java.util.LinkedList;
+import java.util.List;
+
+class ch15 {
+
+  public static void main(String[] args) {
+    List<String> s = new LinkedList<>();
+    s.add("Hello");
+    
+    System.out.println(s.get(0));
+  }
+}
