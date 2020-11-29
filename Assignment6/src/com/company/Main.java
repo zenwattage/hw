6ba0f1de3087bd8ac9cb3bd2b229728f165ce816
Scott H <zenwattage@gmail.com>
2020-11-29 12:32:43 -0800

    renamed list

--- Assignment6/src/com/company/Main.java
+++ Assignment6/src/com/company/Main.java
@@ -16,28 +16,21 @@ public class Main {
 	// write your code here
     // [1, 2, 3, 4, 2, 3, 3]
         //int[] arr =  {1, 2, 3, 4, 2, 3, 3};
-        ArrayIntList a = new ArrayIntList();
-        a.add(1);
-        a.add(2);
-        a.add(3);
-        a.add(4);
-        a.add(2);
-        a.add(3);
-        a.add(3);
+        ArrayIntList list = new ArrayIntList();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(2);
+        list.add(3);
+        list.add(3);
 
-        Client client = new Client(a);
-        System.out.println(client);
-        System.out.println(a);
-        System.out.println("list.lastIndexOf(2) is " + a.lastIndexOf(2));
-        System.out.println("list.count(2) is " + a.count(2));
-//        System.out.println(a.lastIndexOf(2));
-//        System.out.println(a.lastIndexOf(3));
-//        System.out.println("count of 3: " + a.count(3));
-//        System.out.println("count of 2: " + a.count(2));
-        a.removeLast();
+        System.out.println("list = " + list);
+        System.out.println("list.lastIndexOf(2) is " + list.lastIndexOf(2));
+        System.out.println("list.count(2) is " + list.count(2));
+        list.removeLast();
         System.out.println("After a.removeLast()");
-        System.out.println(a);
-        //System.out.println("A to String: " + a.toString());
+        System.out.println(list);
 
     }
 }
