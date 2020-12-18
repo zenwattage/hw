07002a90f192cef0ebaf709e7ea331af23dc9ec6
Scott H <zenwattage@gmail.com>
2020-12-17 16:17:00 -0800

    upping final class assign

--- Testing/src/com/company/Main.java
+++ Testing/src/com/company/Main.java
@@ -27,65 +27,18 @@ public class Main {
     }
 
     public static void main(String[] args) {
-	// write your code here
-    int[] scores = {62, 35, 13, 40, 20};
 
-
-    int search = Arrays.binarySearch(scores,20);
-        System.out.println(search);
-//        for(int each : scores){
-//            System.out.println(each);
-//        }
-//
-//        Arrays.sort(scores);
-//
-//        for(int each : scores){
-//            System.out.println(each);
-//        }
-
-        double[] list = {3.1, 3.1, 2.5, 6.4, 2.1};
-
-        selectionSort(list);
-        for(double each: list){
-            System.out.println(each);
-        }
-
-//        Stack<String> s = new Stack<>();
-//
-//        Queue<String> q = new LinkedList<>();
-//
-//        s.push("how");
-//
-//        s.push("are");
-//
-//        s.push("you");
-//
-//        while (!s.isEmpty()){
-//
-//            q.add(s.pop());
-//
-//        }
-//
-//        System.out.println(q);
-        Stack<Integer> s = new Stack<>();
-
-        s.push(7);
-
-        s.push(10);
-
-        System.out.print(s.pop());
-
-        System.out.print(s.peek());
-
-        s.push(3);
-
-        s.push(5);
-
-        System.out.print(s.pop());
-
-        System.out.print(s.isEmpty());
-
-
-
+        Queue <Integer> q = new LinkedList<>();
+        q.add(3);
+        q.add(5);
+        q.add(9);
+        q.add(q.remove());
+        q.add(2);
+        q.add(4);
+        q.add(q.remove());
+        q.add(q.remove());
+        q.add(1);
+        q.add(8);
+        System.out.println(q.toString());
     }
 }
