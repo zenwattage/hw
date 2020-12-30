cf87b6d8eb2d68ce43895453c85be6bbd0ecf822
Scott H <zenwattage@gmail.com>
2020-12-30 11:19:34 -0800

    adding new prac

---
+++ AD325/multiplyEvens/src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+/**
+ *
+ Write a method multiplyEvens that returns the product of the first n even integers.
+ For example:
+ Call 	Output 	Reason
+ multiplyEvens(1); 	2 	2 = 2
+ multiplyEvens(2); 	8 	2 * 4 = 8
+ multiplyEvens(3); 	48 	2 * 4 * 6 = 48
+ multiplyEvens(4); 	384 	2 * 4 * 6 * 8 = 384
+
+ You should throw an IllegalArgumentException if passed a value less than or equal to 0.
+
+ */
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
